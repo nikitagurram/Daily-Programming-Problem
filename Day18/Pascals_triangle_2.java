4bb14cc6cd06b13f7549f8ee99fa2f721adea3cb
Nikita <nikitagurram28@gmail.com>
2023-10-16 21:27:00 +0530

    Day18 Programs

--- Day18/Pascals_triangle_2.java
+++ Day18/Pascals_triangle_2.java
@@ -1,29 +1,27 @@
 
-/*Given an integer numRows, return the first numRows of Pascal's triangle.
+/*Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
 In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
-Input: numRows = 5
-Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]] */
+Input: rowIndex = 3
+Output: [1,3,3,1]
+Input: rowIndex = 0
+Output: [1] */
 import java.util.ArrayList;
 import java.util.List;
 
-public class Pascals_triangle {
+public class Pascals_triangle_2 {
     public static void main(String[] args) {
-        int numRows = 5;
-        List<List<Integer>> list = new ArrayList<>();
+        int rowIndex = 3;
         List<Integer> prev = new ArrayList<>();
         prev.add(1);
-        list.add(prev);
-        for (int i = 1; i < numRows; i++) {
+        for (int i = 0; i < rowIndex; i++) {
             List<Integer> curr = new ArrayList<>();
             curr.add(1);
             for (int j = 0; j < prev.size() - 1; j++) {
                 curr.add(prev.get(j) + prev.get(j + 1));
             }
             curr.add(1);
-            list.add(curr);
             prev = curr;
         }
-        System.out.println(list);
-
+        System.out.println(prev);
     }
 }
