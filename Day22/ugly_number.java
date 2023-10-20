8e955829a3751620505c5ac79efa022fe5fb1fc2
Nikita <nikitagurram28@gmail.com>
2023-10-20 19:51:05 +0530

    Day22 Programs

---
+++ Day22/ugly_number.java
@@ -0,0 +1,20 @@
+package Day22;
+
+/*An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+Given an integer n, return true if n is an ugly number.
+Input: n = 6
+Output: true
+Explanation: 6 = 2 × 3
+Input: n = 1
+Output: true
+Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5. */
+public class ugly_number {
+    public static void main(String[] args) {
+        int n = 6;
+        for (int i = 2; i < 6 && n > 0; i++) {
+            while (n % i == 0)
+                n /= i;
+        }
+        System.out.println(n == 1);
+    }
+}
