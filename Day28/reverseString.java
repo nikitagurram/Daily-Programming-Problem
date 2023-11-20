e05e0ca733595c4a1fdec43335b1faa97a6e98dd
Nikita <nikitagurram28@gmail.com>
2023-11-20 21:33:28 +0530

    Day28 program

--- Day28/reverseString.java
+++ Day28/reverseString.java
@@ -1,11 +1,12 @@
 public class reverseString {
     public static void main(String[] args) {
-
         String str = "Geeks";
-        String revStr = "";
-        for (int i = 0; i < str.length(); i++) {
-            revStr = str.charAt(i) + revStr;
-        }
-        System.out.println(revStr);
+        StringBuilder revstr= new StringBuilder(str);
+		revstr.reverse();
+        // String revStr = "";
+        // for (int i = 0; i < str.length(); i++) {
+        //     revStr = str.charAt(i) + revStr;
+        // }
+        System.out.println(revstr.toString());
     }
 }
