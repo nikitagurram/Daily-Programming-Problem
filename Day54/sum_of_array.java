ba52e092c72eb68b329a9553a36dc90008555648
Nikita <nikitagurram28@gmail.com>
2023-11-21 19:21:48 +0530

    Day54 program

---
+++ Day54/sum_of_array.java
@@ -0,0 +1,12 @@
+package Day54;
+
+public class sum_of_array {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
