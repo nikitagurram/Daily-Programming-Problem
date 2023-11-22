06991cba521a8bbf68483fd098c115ae42b843f4
Nikita <nikitagurram28@gmail.com>
2023-11-22 20:08:34 +0530

    Day55 program

---
+++ Day55/sum_of_even_odd.java
@@ -0,0 +1,28 @@
+package Day55;
+
+import java.util.Scanner;
+
+/*White a program to input on integer n and print the sum of of its even digits and the sum of all its odd digits separately.
+Digits mean numbers, not places! That is, if the given integer is "132456" even digits are 2. 4, and 6, and odd digits are 1, 3. and 5.
+Input: 'n' = 132456
+Output: 129
+Explanation:
+The sum of even digits = 2+4+6=12
+The sum of odd digits = 1+3+5=9 */
+public class sum_of_even_odd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int even = 0;
+        int odd = 0;
+        while (n != 0) {
+            int rem = n % 10;
+            if (rem % 2 == 0)
+                even += rem;
+            else
+                odd += rem;
+            n /= 10;
+        }
+        System.out.println(even + " " + odd);
+    }
+}
