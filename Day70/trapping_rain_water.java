d8761d0edfc54e8a75a5da8604a83601bce2868a
Nikita <nikitagurram28@gmail.com>
2023-12-07 20:00:58 +0530

    Day70 program

---
+++ Day70/trapping_rain_water.java
@@ -0,0 +1,31 @@
+package Day70;
+
+/*Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining
+Example 1:
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+Example 2:
+Input: height = [4,2,0,3,2,5]
+Output: 9 */
+public class trapping_rain_water {
+    public static void main(String[] args) {
+        int height[] = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+        int ans = 0;
+        int size = height.length;
+        int left_max[] = new int[size];
+        int right_max[] = new int[size];
+        left_max[0] = height[0];
+        for (int i = 1; i < size; i++) {
+            left_max[i] = Math.max(height[i], left_max[i - 1]);
+        }
+        right_max[size - 1] = height[size - 1];
+        for (int i = size - 2; i >= 0; i--) {
+            right_max[i] = Math.max(height[i], right_max[i + 1]);
+        }
+        for (int i = 0; i < size; i++) {
+            ans += Math.min(left_max[i], right_max[i]) - height[i];
+        }
+        System.out.println(ans);
+    }
+}
