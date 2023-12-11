44356e250e42f18f52e31fb1e8fd124c8acb343b
Nikita <nikitagurram28@gmail.com>
2023-12-11 19:42:33 +0530

    Day74 program

--- Day74/element_appearing_more_than_25_in_sorted_array.java
+++ Day74/element_appearing_more_than_25_in_sorted_array.java
@@ -1,5 +1,12 @@
 package Day74;
 
+/*Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time, return that integer.
+Example 1:
+Input: arr = [1,2,2,6,6,6,6,7,10]
+Output: 6
+Example 2:
+Input: arr = [1,1]
+Output: 1 */
 public class element_appearing_more_than_25_in_sorted_array {
     public static void main(String[] args) {
         int arr[] = { 1, 2, 2, 6, 6, 6, 6, 7, 9 };
